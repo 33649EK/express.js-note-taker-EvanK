d1504fdde34568f14bb2e37595e6466b307835d1
Evan Keller <33649ek@gmail.com>
2024-01-12 19:30:13 -0700

    heroku fix 9

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get("/notes", (req, res) =>
 );
 
 // Default route for handling all other requests
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
